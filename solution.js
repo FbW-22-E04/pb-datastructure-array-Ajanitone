a4dc277f33b68660173c9e2635470b6358db855e
Ajanitone <ajanitone@freenet.de>
2022-07-11 11:45:03 +0200

    pb-data-array

---
+++ solution.js
@@ -0,0 +1,19 @@
+// 1.Declare a variable named myArray and assign an array that contains 5 elements. Print the array.
+
+const myArray = [1, 2, 3, 4, 5];
+console.log("01: myArray printed", myArray);
+
+console.log("-------------------------------------------------");
+
+// 2.Access the 3rd element in the array and save that to another variable named thirdElement. Print thirdElement.
+
+const thirdElement = myArray[2];
+console.log("02:thirdElement", thirdElement);
+
+console.log("-------------------------------------------------");
+
+// 03.Reassign the fourth element of the array and give it a value of "fourth element". Print the array once more.
+
+myArray[3] = "fourth element";
+
+console.log("03:myArray fourth element reassigned", myArray);
